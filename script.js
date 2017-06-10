bb6b138928337bd470f5460604c266efe1fb9ba3
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-06-10 15:32:50 -0400

    Add logic to switch between pictures

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 $(document).ready(function(){
+  var count = 0;
+  $("#carousel").html(pictures[0])
 
-$("button").on('click',function() {
-  $("#carousel").append(pictures[0])
+$(".fa-arrow-circle-right").on('click',function() {
+  count++
+
+  $("#carousel").html(pictures[count])
+  if (count == 5) {
+  count = -1 }
+
+  if (count == 6){
+  $("#carousel").html(pictures[0])
+  count = 0; }
+
+})
+
+$(".fa-arrow-circle-left").on('click',function() {
+  count--
+  $("#carousel").html(pictures[count])
+
+  if (count == -1){
+    $("#carousel").html(pictures[5])
+    count = 5; }
+
 })
 
 })
 
 var pictures = [
   "<img src='http://www.almanac.com/sites/default/files/birth_month_flowers-primary-1920x1280px_pixabay.jpg' height='200' width='100'>",
-  "<img src='https://cdn.pixabay.com/photo/2016/07/04/18/16/sun-flower-1497092_960_720.jpg' height='200' width='100'>"
+  "<img src='https://cdn.pixabay.com/photo/2016/07/04/18/16/sun-flower-1497092_960_720.jpg' height='200' width='100'>",
+    "<img src='https://static.pexels.com/photos/87452/flowers-background-butterflies-beautiful-87452.jpeg' height='200' width='100'>",
+      "<img src='http://en.bcdn.biz/Images/2016/7/11/c86de5a5-2107-4382-a98c-184ebf681497.jpg' height='200' width='100'>",
+        "<img src='https://static.pexels.com/photos/101472/pexels-photo-101472.jpeg' height='200' width='100'>",
+        "<img src='http://www.hdwallpapers.in/walls/pink_cosmos_flowers-wide.jpg' height='200' width='100'>"
 ]
