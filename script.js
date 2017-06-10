02ec37e11660426428fbd381862eb40973bee668
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-06-10 14:42:26 -0400

    Change pictures object to array

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
 
 $("button").on('click',function() {
-  $("#carousel").append(pictures.pic1)
+  $("#carousel").append(pictures[0])
 })
 
 })
 
-var pictures = {
-  pic1: "<img src='http://www.almanac.com/sites/default/files/birth_month_flowers-primary-1920x1280px_pixabay.jpg' height='200' width='100'>",
-  pic2: "<img src='https://cdn.pixabay.com/photo/2016/07/04/18/16/sun-flower-1497092_960_720.jpg' height='200' width='100'>",
-
-
-}
+var pictures = [
+  "<img src='http://www.almanac.com/sites/default/files/birth_month_flowers-primary-1920x1280px_pixabay.jpg' height='200' width='100'>",
+  "<img src='https://cdn.pixabay.com/photo/2016/07/04/18/16/sun-flower-1497092_960_720.jpg' height='200' width='100'>"
+]
