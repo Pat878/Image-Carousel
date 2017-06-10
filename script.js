2d638d0a4fa3b0358fb8fa70c82de88c73a0c006
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-06-10 14:37:31 -0400

    Update append function

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-window.onload=function(){
-  var pic = "http://www.almanac.com/sites/default/files/birth_month_flowers-primary-1920x1280px_pixabay.jpg"
-var elem = document.createElement("img");
-elem.setAttribute("src", pic);
-elem.setAttribute("height", "200");
-elem.setAttribute("width", "200");
-//elem.setAttribute("alt", "Flower");
-document.getElementById("carousel").appendChild(elem);
+$(document).ready(function(){
+
+$("button").on('click',function() {
+  $("#carousel").append(pictures.pic1)
+})
+
+})
+
+var pictures = {
+  pic1: "<img src='http://www.almanac.com/sites/default/files/birth_month_flowers-primary-1920x1280px_pixabay.jpg' height='200' width='100'>",
+  pic2: "<img src='https://cdn.pixabay.com/photo/2016/07/04/18/16/sun-flower-1497092_960_720.jpg' height='200' width='100'>",
+
+
 }
