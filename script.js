66dce318545fd5813e4ab52fbbfb45651ab1321f
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-06-11 11:47:33 -0400

    Remove console.log

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 
   timer = setInterval(function(){
 
-  console.log(count)
     count++
     $("#carousel").html($(pictures[count]).fadeIn())
     $(".circle").removeClass("active")
